6a5db91a3da74acc276f64a3dc0c92b650d1ce49
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2024-08-27 20:49:16 -0700

    Added tests

--- src/perft.rs
+++ src/perft.rs
@@ -154,4 +154,22 @@ mod tests {
         let fen = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1";
         generic_depth_test(Some(fen), 4);
     }
+
+    #[test]
+    fn test_p3_depth_5() {
+        let fen = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1";
+        generic_depth_test(Some(fen), 5);
+    }
+
+    #[test]
+    fn test_p4_depth_4() {
+        let fen = "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1";
+        generic_depth_test(Some(fen), 4);
+    }
+
+    #[test]
+    fn test_p5_depth_4() {
+        let fen = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8";
+        generic_depth_test(Some(fen), 4);
+    }
 }
