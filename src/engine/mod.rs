a1dca6f9c7008e7fef03aa2629ae18c439a92242
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2024-10-29 02:46:42 -0700

    Implemented custom MCTS

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -6,8 +6,12 @@ use std::cell::RefCell;
 use std::rc::{Rc, Weak};
 
 fn ucb1(node_value: f64, parent_visits: u32, child_visits: u32, exploration_param: f64) -> f64 {
-    node_value / child_visits as f64
-        + exploration_param * ((parent_visits as f64).ln() / child_visits as f64).sqrt()
+    if child_visits == 0 {
+        f64::INFINITY
+    } else {
+        node_value / child_visits as f64
+            + exploration_param * ((parent_visits as f64).ln() / child_visits as f64).sqrt()
+    }
 }
 
 fn simulate_rollout(mut state: State) -> f64 {
@@ -16,7 +20,7 @@ fn simulate_rollout(mut state: State) -> f64 {
     loop {
         let moves = state.calc_legal_moves();
         if moves.is_empty() {
-            return evaluate_game_state(&state, current_side_to_move);
+            return evaluate_terminal_state(&state, current_side_to_move);
         } else {
             let rand_idx = rng.usize(..moves.len());
             let mv = moves[rand_idx];
@@ -25,9 +29,16 @@ fn simulate_rollout(mut state: State) -> f64 {
     }
 }
 
-fn evaluate_game_state(state: &State, for_color: Color) -> f64 {
-    match state.termination {
-        None | Some(Termination::Checkmate) => {
+fn evaluate_terminal_state(state: &State, for_color: Color) -> f64 {
+    let termination = match &state.termination {
+        Some(termination) => termination,
+        None => &match state.board.is_color_in_check(state.side_to_move) {
+            true => Termination::Checkmate,
+            false => Termination::Stalemate,
+        }
+    };
+    match termination {
+        Termination::Checkmate => {
             let checkmated_side = state.side_to_move;
             if checkmated_side == for_color {
                 0.0
@@ -35,7 +46,10 @@ fn evaluate_game_state(state: &State, for_color: Color) -> f64 {
                 1.0
             }
         }
-        Some(_) => 0.5,
+        term => {
+            println!("{:?}", term);
+            0.5
+        }
     }
 }
 
@@ -43,8 +57,8 @@ struct MCTSNode {
     state: State,
     visits: u32,
     value: f64,
-    children: Vec<Rc<RefCell<MCTSNode>>>,
-    parent: Option<Weak<RefCell<MCTSNode>>>,
+    children: Vec<*mut MCTSNode>,
+    parent: Option<*mut MCTSNode>,
 }
 
 impl MCTSNode {
@@ -58,15 +72,14 @@ impl MCTSNode {
         }
     }
 
-    fn run(&mut self, exploration_param: f64, self_as_parent: Weak<RefCell<MCTSNode>>) {
+    fn run(&mut self, exploration_param: f64, self_ptr: *mut MCTSNode) {
         let possible_selected_node = self.select_child_with_ucb1(exploration_param);
 
         match possible_selected_node {
-            Some(selected_node) => {
-                // Upgrade the weak reference temporarily for recursive call
-                selected_node.borrow_mut().run(exploration_param, Rc::downgrade(&selected_node));
+            Some(selected_node) => unsafe {
+                (*selected_node).run(exploration_param, selected_node);
             }
-            None => {
+            None => unsafe {
                 if self.visits == 0 {
                     let value = simulate_rollout(self.state.clone());
                     self.backpropagate(value);
@@ -76,66 +89,77 @@ impl MCTSNode {
                         let mut new_state = self.state.clone();
                         new_state.make_move(legal_move);
                         let mut new_node = MCTSNode::new(new_state);
-                        new_node.parent = Some(self_as_parent.clone()); // Pass weak reference
-                        self.children.push(Rc::new(RefCell::new(new_node)));
+                        new_node.parent = Some(self_ptr);
+                        self.children.push(Box::into_raw(Box::new(new_node)));
                     }
                     if !self.children.is_empty() {
                         let random_child = self.children[fastrand::usize(..self.children.len())].clone();
-                        random_child.borrow_mut().run(exploration_param, Rc::downgrade(&random_child));
+                        (*random_child).run(exploration_param, random_child);
                     }
                 }
             }
         }
     }
 
-    fn select_child_with_ucb1(&mut self, exploration_param: f64) -> Option<Rc<RefCell<MCTSNode>>> {
-        self.children.iter()
-            .max_by(|a, b| {
-                ucb1(a.borrow().value, self.visits, a.borrow().visits, exploration_param)
-                    .partial_cmp(&ucb1(b.borrow().value, self.visits, b.borrow().visits, exploration_param))
-                    .unwrap()
-            })
-            .cloned() // Return an owned Rc reference
+    fn select_child_with_ucb1(&mut self, exploration_param: f64) -> Option<*mut MCTSNode> {
+        unsafe {
+            self.children.iter().max_by(|a, b| {
+                let a_ucb1 = ucb1((***a).value, self.visits, (***a).visits, exploration_param);
+                let b_ucb1 = ucb1((***b).value, self.visits, (***b).visits, exploration_param);
+                a_ucb1.partial_cmp(&b_ucb1).expect("Failed to compare UCB1 values")
+            }).cloned()
+        }
     }
 
     fn backpropagate(&mut self, value: f64) {
         self.visits += 1;
         self.value += value;
 
-        // Clone the parent reference to extend its lifetime beyond the mutable borrow
-        let parent_option = self.parent.clone();
+        if let Some(parent_ptr) = &self.parent {
+            unsafe { (**parent_ptr).backpropagate(value) };
+        }
+    }
+}
 
-        // Mutable borrow ends here
-
-        if let Some(weak_parent) = parent_option {
-            if let Some(parent_rc) = weak_parent.upgrade() {
-                parent_rc.borrow_mut().backpropagate(value);
+impl Drop for MCTSNode {
+    fn drop(&mut self) {
+        for child in self.children.iter() {
+            unsafe {
+                let _ = Box::from_raw(*child);
             }
         }
     }
 }
 
 struct MCTS {
-    root: Rc<RefCell<MCTSNode>>,
+    root: *mut MCTSNode,
     exploration_param: f64,
 }
 
 impl MCTS {
     fn new(state: State, exploration_param: f64) -> Self {
         Self {
-            root: Rc::new(RefCell::new(MCTSNode::new(state))),
+            root: Box::into_raw(Box::new(MCTSNode::new(state))),
             exploration_param,
         }
     }
 
     fn run(&mut self, iterations: u32) {
         for _ in 0..iterations {
-            self.root.borrow_mut().run(self.exploration_param, Rc::downgrade(&self.root));
+            unsafe { (*self.root).run(self.exploration_param, self.root) };
         }
     }
 
-    fn select_best_move(&self) -> Option<Rc<RefCell<MCTSNode>>> {
-        self.root.borrow().children.iter().max_by_key(|child| child.borrow().visits).cloned()
+    fn select_best_move(&self) -> Option<*mut MCTSNode> {
+        unsafe { (*self.root).children.iter().max_by_key(|child| (***child).visits).cloned() }
+    }
+}
+
+impl Drop for MCTS {
+    fn drop(&mut self) {
+        unsafe {
+            let _ = Box::from_raw(self.root);
+        }
     }
 }
 
@@ -146,9 +170,17 @@ mod tests {
     #[test]
     fn test_mcts() {
         let mut mcts = MCTS::new(State::initial(), 1.41);
-        mcts.run(1000);
-        if let Some(best_move) = mcts.select_best_move() {
-            best_move.borrow().state.board.print();
+        for i in 0..10 {
+            println!("Iteration: {}", i);
+            mcts.run(100);
+            if let Some(best_move) = mcts.select_best_move() {
+                let next_state = unsafe { (*best_move).state.clone() };
+                mcts = MCTS::new(next_state.clone(), 1.41);
+                next_state.board.print();
+            }
+            else{
+                break;
+            }
         }
     }
 }
