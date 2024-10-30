ec17636d6e04b524f54d2471ed7abaa8ed80e483
Vibhav Singamshetty <vibsingamshetty@gmail.com>
2024-10-29 18:53:13 -0700

    Improved MCTS

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,19 +1,18 @@
+mod evaluate;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+use crate::engine::evaluate::evaluate_non_terminal_state;
 use crate::r#move::Move;
-use crate::state::{State, Termination};
+use crate::state::{Context, State, Termination};
 use crate::utils::Color;
 
-fn ucb1(node_value: f64, parent_visits: u32, child_visits: u32, exploration_param: f64) -> f64 {
-    if child_visits == 0 {
-        f64::INFINITY
-    } else {
-        node_value / child_visits as f64
-            + exploration_param * ((parent_visits as f64).ln() / child_visits as f64).sqrt()
-    }
-}
+const MAX_ROLLOUT_DEPTH: u32 = 50;
 
 fn simulate_rollout(mut state: State) -> f64 {
     let current_side_to_move = state.side_to_move;
     let mut rng = fastrand::Rng::new();
+    let mut i = 0;
     loop {
         let moves = state.calc_legal_moves();
         if moves.is_empty() {
@@ -23,16 +22,20 @@ fn simulate_rollout(mut state: State) -> f64 {
             let mv = moves[rand_idx];
             state.make_move(mv);
         }
+        i += 1;
+        if i >= MAX_ROLLOUT_DEPTH {
+            return evaluate_non_terminal_state(&state, current_side_to_move);
+        }
     }
 }
 
 fn evaluate_terminal_state(state: &State, for_color: Color) -> f64 {
-    state.board.print();
-    println!("{:?}", state.termination);
-    println!("{:?}", state.side_to_move);
-    println!("{:?}", state.halfmove);
-    println!("{:?}", state.context.borrow().halfmove_clock);
-    println!();
+    // state.board.print();
+    // println!("{:?}", state.termination);
+    // println!("{:?}", state.side_to_move);
+    // println!("{:?}", state.halfmove);
+    // println!("{:?}", state.context.borrow().halfmove_clock);
+    // println!();
     let termination = match &state.termination {
         Some(termination) => termination,
         None => &match state.board.is_color_in_check(state.side_to_move) {
@@ -54,6 +57,7 @@ fn evaluate_terminal_state(state: &State, for_color: Color) -> f64 {
     }
 }
 
+#[derive(Debug)]
 struct MCTSNode {
     state: State,
     visits: u32,
@@ -76,10 +80,10 @@ impl MCTSNode {
         let value;
 
         match possible_selected_child {
-            Some(selected_child) => unsafe {
+            Some(selected_child) => unsafe { // has at least one child
                 value = (*selected_child).run(exploration_param);
             }
-            None => unsafe {
+            None => unsafe { // no children
                 if self.visits == 0 {
                     value = simulate_rollout(self.state.clone());
                 } else {
@@ -106,7 +110,12 @@ impl MCTSNode {
     }
     
     fn calc_ucb1(&self, parent_visits: u32, exploration_param: f64) -> f64 {
-        ucb1(self.value, parent_visits, self.visits, exploration_param)
+        if self.visits == 0 {
+            f64::INFINITY
+        } else {
+            self.value / self.visits as f64
+                + exploration_param * ((parent_visits as f64).ln() / self.visits as f64).sqrt()
+        }
     }
 
     fn select_child_with_ucb1(&mut self, exploration_param: f64) -> Option<*mut MCTSNode> {
@@ -164,15 +173,24 @@ impl Drop for MCTS {
 
 #[cfg(test)]
 mod tests {
+    use std::thread;
     use super::*;
+
+    fn run_simulations(state: State, iterations: u32, exploration_param: f64) -> MCTSNode {
+        let mut root = MCTSNode::new(state);
+        for _ in 0..iterations {
+            root.run(exploration_param);
+        }
+        root
+    }
 
     #[test]
     fn test_mcts() {
         let exploration_param = 1.41;
-        let mut mcts = MCTS::new(State::initial(), exploration_param);
+        let mut mcts = MCTS::new(State::from_fen("r1n1k3/p2p1pbr/B1p1pnp1/2qPN3/4P3/R1N1BQ1P/1PP2P1P/4K2R w Kq - 3 6").unwrap(), exploration_param);
         for i in 0..10 {
-            println!("Iteration: {}", i);
-            mcts.run(100);
+            println!("Move: {}", i);
+            mcts.run(1000);
             if let Some(best_move) = mcts.select_best_move() {
                 let next_state = unsafe { (*best_move).state.clone() };
                 mcts = MCTS::new(next_state.clone(), exploration_param);
